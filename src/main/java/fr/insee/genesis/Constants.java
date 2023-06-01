edad1c67667188f2fa2baae8bc98edd3dab8c0fa
Loïc Henninger <loic.henninger@insee.fr>
2023-06-01 10:48:25 +0200

    Initialize applications.properties and Constants class

---
+++ src/main/java/fr/insee/genesis/Constants.java
@@ -0,0 +1,11 @@
+package fr.insee.genesis;
+
+public class Constants {
+
+    public static final String XSLT_STRUCTURED_VARIABLES = "xslt/structured-variables.xsl";
+
+    public static final String ROOT_GROUP_NAME = "RACINE";
+    public static final String ROOT_IDENTIFIER_NAME = "IdUE";
+    public static final String METADATA_SEPARATOR = ".";
+
+}
