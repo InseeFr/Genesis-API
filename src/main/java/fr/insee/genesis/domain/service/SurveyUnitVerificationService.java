8c4ccfb5873087a01ced1059a70d89d3a677927f
QDIBYS <alexis.szmundy@insee.fr>
2023-11-20 16:33:58 +0100

    Fixes on API + removed functionnal tests for now

--- src/main/java/fr/insee/genesis/domain/service/SurveyUnitVerificationService.java
+++ src/main/java/fr/insee/genesis/domain/service/SurveyUnitVerificationService.java
@@ -3,12 +3,14 @@ package fr.insee.genesis.domain.service;
 import fr.insee.genesis.controller.sources.ddi.VariablesMap;
 import fr.insee.genesis.controller.utils.DataVerifier;
 import fr.insee.genesis.domain.dtos.SurveyUnitUpdateDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * This service is used in the data verification process
  */
+@Service
 public class SurveyUnitVerificationService {
     public void verifySurveyUnits(List<SurveyUnitUpdateDto> suDtos, VariablesMap variablesMap) {
         DataVerifier.verifySurveyUnits(suDtos,variablesMap);
--- src/test/java/cucumber/CucumberSpringConfiguration.java
+++
@@ -1,9 +0,0 @@
-package cucumber;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class CucumberSpringConfiguration {
-}
