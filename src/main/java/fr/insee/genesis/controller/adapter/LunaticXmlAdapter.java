e7c3ea72ede001d0ceed467834425232def32da0
QDIBYS <alexis.szmundy@insee.fr>
2024-02-23 10:02:15 +0100

    Renamed getParentGroupName to getParentName + extract method

--- src/main/java/fr/insee/genesis/controller/adapter/LunaticXmlAdapter.java
+++ src/main/java/fr/insee/genesis/controller/adapter/LunaticXmlAdapter.java
@@ -106,7 +106,7 @@ public class LunaticXmlAdapter {
                                 .idVar(lunaticXmlCollectedData.getVariableName())
                                 .values(variableValues)
                                 .idLoop(LoopIdentifier.getLoopIdentifier(lunaticXmlCollectedData.getVariableName(), variablesMap, i))
-                                .idParent(LoopIdentifier.getParentGroupName(lunaticXmlCollectedData.getVariableName(), variablesMap))
+                                .idParent(LoopIdentifier.getParentName(lunaticXmlCollectedData.getVariableName(), variablesMap))
                                 .build());
                         dataCount++;
                     }
--- src/main/java/fr/insee/genesis/controller/utils/LoopIdentifier.java
+++ src/main/java/fr/insee/genesis/controller/utils/LoopIdentifier.java
@@ -21,14 +21,12 @@ public class LoopIdentifier {
 			if(variableName.startsWith(Constants.FILTER_RESULT_PREFIX)
 					&& variablesMap.hasVariable(variableName.replace(Constants.FILTER_RESULT_PREFIX,""))
 			){
-				Variable parentVariable = variablesMap.getVariable(variableName.replace(Constants.FILTER_RESULT_PREFIX,""));
-				return parentVariable.getGroupName();
+				return getParentGroupName(variablesMap, variableName, Constants.FILTER_RESULT_PREFIX);
 			}
 			if(variableName.endsWith(Constants.MISSING_SUFFIX)
 					&& variablesMap.hasVariable(variableName.replace(Constants.MISSING_SUFFIX,""))
 			){
-				Variable parentVariable = variablesMap.getVariable(variableName.replace(Constants.MISSING_SUFFIX,""));
-				return parentVariable.getGroupName();
+				return getParentGroupName(variablesMap, variableName, Constants.MISSING_SUFFIX);
 			}
 			log.debug("Variable {} not found in variablesMap and assigned in root group", variableName);
 			return Constants.ROOT_GROUP_NAME;
@@ -39,7 +37,12 @@ public class LoopIdentifier {
 		return String.format("%s_%d", variable.getGroup().getName() ,index);
 	}
 
-	public static String getParentGroupName(String variableName, VariablesMap variablesMap) {
+	private static String getParentGroupName(VariablesMap variablesMap, String variableName, String constantToReplace) {
+		Variable parentVariable = variablesMap.getVariable(variableName.replace(constantToReplace, ""));
+		return parentVariable.getGroupName();
+	}
+
+	public static String getParentName(String variableName, VariablesMap variablesMap) {
 		Variable variable = variablesMap.getVariable(variableName);
 		List<String> varsEno = Arrays.asList(Constants.getEnoVariables());
 		if ( variable == null ) {
