71842b1930fa7a23c447ec022d42c7370f8c5122
QDIBYS <alexis.szmundy@insee.fr>
2024-04-11 14:46:53 +0200

    Hide Bangles only operations

--- src/main/java/fr/insee/genesis/controller/rest/ScheduleController.java
+++ src/main/java/fr/insee/genesis/controller/rest/ScheduleController.java
@@ -37,7 +37,7 @@ public class ScheduleController {
 
     //TODO French traduction for users
 
-    @Operation(summary = "Fetch all schedules (for Bangles only)")
+    @Operation(summary = "Fetch all schedules (for Bangles only)", hidden = true)
     @GetMapping(path = "/all")
     public ResponseEntity<Object> getAllSchedules() {
         log.debug("Got GET all schedules request");
@@ -69,7 +69,7 @@ public class ScheduleController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @Operation(summary = "Update last execution date (for Bangles only)")
+    @Operation(summary = "Update last execution date (for Bangles only)", hidden = true)
     @PostMapping(path = "/updateLastExecutionDate")
     public ResponseEntity<Object> updateSurveyLastExecution(
             @Parameter(description = "Survey name to call Kraftwerk on") @RequestBody String surveyName
