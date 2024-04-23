562fe5fda9e401eca3d0b51f1653bff974e3fa02
QDIBYS <alexis.szmundy@insee.fr>
2024-04-23 09:03:59 +0200

    Removed unused imports

--- src/main/java/fr/insee/genesis/controller/rest/ScheduleController.java
+++ src/main/java/fr/insee/genesis/controller/rest/ScheduleController.java
@@ -22,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
 
--- src/main/java/fr/insee/genesis/infrastructure/model/document/schedule/KraftwerkExecutionSchedule.java
+++ src/main/java/fr/insee/genesis/infrastructure/model/document/schedule/KraftwerkExecutionSchedule.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.nio.file.Path;
 import java.time.LocalDateTime;
 
 @Data
