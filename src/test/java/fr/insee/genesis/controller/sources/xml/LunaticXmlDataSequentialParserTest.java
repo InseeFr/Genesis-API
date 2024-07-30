271866172415c7609a074e08e0317eb73a140fef
Alice Lambois <alice.lambois@insee.fr>
2024-07-30 16:47:24 +0200

    Update changelog and refactor for sonar

--- src/test/java/fr/insee/genesis/controller/sources/xml/LunaticXmlDataSequentialParserTest.java
+++ src/test/java/fr/insee/genesis/controller/sources/xml/LunaticXmlDataSequentialParserTest.java
@@ -33,25 +33,25 @@ class LunaticXmlDataSequentialParserTest {
     // Then
 
     @Test
-    void campaignHasGoodLabelAndId() throws Exception {
+    void campaignHasGoodLabelAndId()  {
         Assertions.assertThat(campaign.getLabel()).isEqualTo("Enquête Test Unitaire");
         Assertions.assertThat(campaign.getIdCampaign()).isEqualTo("TEST2023X01");
     }
 
     @Test
-    void hasOneUE() throws Exception {
+    void hasOneUE()  {
         Assertions.assertThat(surveyUnit).isNotNull();
         Assertions.assertThat(surveyUnit.getId()).isEqualTo("UE0000000001");
         Assertions.assertThat(surveyUnit.getQuestionnaireModelId()).isEqualTo("TEST2023X01");
     }
 
     @Test
-    void checkNumberOfCollectedVariables() throws Exception {
+    void checkNumberOfCollectedVariables()  {
         Assertions.assertThat(surveyUnit.getData().getCollected()).hasSize(26);
     }
 
     @Test
-    void checkNumberOfExternalVariables() throws Exception {
+    void checkNumberOfExternalVariables()  {
         Assertions.assertThat(surveyUnit.getData().getExternal()).hasSize(11);
     }
 
@@ -63,22 +63,22 @@ class LunaticXmlDataSequentialParserTest {
         Assertions.assertThat(surveyUnit.getData().getCollected().get(1)).isNotNull();
         Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected()).isNotEmpty();
         Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected()).hasSize(2);
-        Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected().get(0).getType()).isEqualTo("string");
-        Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected().get(0).getValue()).isEqualTo("012");
+        Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected().getFirst().getType()).isEqualTo("string");
+        Assertions.assertThat(surveyUnit.getData().getCollected().get(1).getCollected().getFirst().getValue()).isEqualTo("012");
 
         Assertions.assertThat(surveyUnit.getData().getCollected().get(4)).isNotNull();
         Assertions.assertThat(surveyUnit.getData().getCollected().get(4).getCollected()).hasSize(1);
-        Assertions.assertThat(surveyUnit.getData().getCollected().get(4).getCollected().get(0).getType()).isEqualTo("string");
-        Assertions.assertThat(surveyUnit.getData().getCollected().get(4).getCollected().get(0).getValue()).isEqualTo("2");
+        Assertions.assertThat(surveyUnit.getData().getCollected().get(4).getCollected().getFirst().getType()).isEqualTo("string");
+        Assertions.assertThat(surveyUnit.getData().getCollected().get(4).getCollected().getFirst().getValue()).isEqualTo("2");
     }
 
     @Test
     void checkExternalVariableValue(){
-        Assertions.assertThat(surveyUnit.getData().getExternal().get(0)).isNotNull();
-        Assertions.assertThat(surveyUnit.getData().getExternal().get(0).getValues()).isNotEmpty();
-        Assertions.assertThat(surveyUnit.getData().getExternal().get(0).getValues()).hasSize(1);
-        Assertions.assertThat(surveyUnit.getData().getExternal().get(0).getValues().get(0).getType()).isEqualTo("string");
-        Assertions.assertThat(surveyUnit.getData().getExternal().get(0).getValues().get(0).getValue()).isEqualTo("BOB");
+        Assertions.assertThat(surveyUnit.getData().getExternal().getFirst()).isNotNull();
+        Assertions.assertThat(surveyUnit.getData().getExternal().getFirst().getValues()).isNotEmpty();
+        Assertions.assertThat(surveyUnit.getData().getExternal().getFirst().getValues()).hasSize(1);
+        Assertions.assertThat(surveyUnit.getData().getExternal().getFirst().getValues().getFirst().getType()).isEqualTo("string");
+        Assertions.assertThat(surveyUnit.getData().getExternal().getFirst().getValues().getFirst().getValue()).isEqualTo("BOB");
     }
 
     @AfterAll
