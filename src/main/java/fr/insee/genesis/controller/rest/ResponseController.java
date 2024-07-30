a7e75448e844954c809feb4cdbf78ad49b0237b7
Alice Lambois <alice.lambois@insee.fr>
2024-07-30 17:10:58 +0200

    Update ResponseController.java

--- src/main/java/fr/insee/genesis/controller/rest/ResponseController.java
+++ src/main/java/fr/insee/genesis/controller/rest/ResponseController.java
@@ -51,7 +51,6 @@ import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.stream.Stream;
 
