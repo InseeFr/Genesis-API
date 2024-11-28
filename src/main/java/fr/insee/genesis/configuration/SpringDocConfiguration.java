7e04a6df9d423b15931bd28101c95db975bf7e5a
Loïc Henninger <loic.henninger@insee.fr>
2024-11-28 15:15:03 +0100

    Add SSO to swagger

--- src/main/java/fr/insee/genesis/configuration/SpringDocConfiguration.java
+++ src/main/java/fr/insee/genesis/configuration/SpringDocConfiguration.java
@@ -3,9 +3,8 @@ package fr.insee.genesis.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.*;
 import io.swagger.v3.oas.models.servers.Server;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
@@ -14,43 +13,61 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringDocConfiguration {
 
-  @Value("${fr.insee.genesis.version}")
-  private String projectVersion;
+    @Value("${fr.insee.genesis.version}")
+    private String projectVersion;
+    public static final String BEARERSCHEME = "bearerAuth";
+    public static final String OAUTH2SCHEME = "oauth2";
 
-  public static final String BEARERSCHEME = "bearerAuth";
+    @Bean
+    @ConditionalOnProperty(name = "fr.insee.genesis.authentication", havingValue = "NONE")
+    public OpenAPI noAuthOpenAPI() {
+        return generateOpenAPI();
+    }
 
-  @Bean
-  @ConditionalOnProperty(name = "fr.insee.genesis.authentication", havingValue = "NONE")
-  public OpenAPI noAuthOpenAPI() {
-    return generateOpenAPI();
-  }
+    @Bean
+    @ConditionalOnProperty(name = "fr.insee.genesis.authentication", havingValue = "OIDC")
+    public OpenAPI oidcOpenAPI(Config config) {
+        String authUrl = config.getAuthServerUrl() + "/realms/" + config.getRealm() + "/protocol/openid-connect";
+        return generateOpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(OAUTH2SCHEME))
+                .addSecurityItem(new SecurityRequirement().addList(BEARERSCHEME))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(OAUTH2SCHEME,
+                                        new SecurityScheme()
+                                                .name(OAUTH2SCHEME)
+                                                .type(SecurityScheme.Type.OAUTH2)
+                                                .flows(getFlows(authUrl))
+                                )
+                                .addSecuritySchemes(BEARERSCHEME,
+                                        new SecurityScheme()
+                                                .name(BEARERSCHEME)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
 
-  @Bean
-  @ConditionalOnProperty(name = "fr.insee.genesis.authentication", havingValue = "OIDC")
-  public OpenAPI oidcOpenAPI() {
-    return generateOpenAPI()
-            .addSecurityItem(new SecurityRequirement().addList(BEARERSCHEME))
-            .components(
-                    new Components()
-                            .addSecuritySchemes(BEARERSCHEME,
-                                    new SecurityScheme()
-                                            .name(BEARERSCHEME)
-                                            .type(SecurityScheme.Type.HTTP)
-                                            .scheme("bearer")
-                                            .bearerFormat("JWT")
-                            )
-            );
-  }
+    private OpenAPI generateOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("/"))
+                .info(new Info()
+                        .title("Genesis API")
+                        .description("Rest Endpoints and services to communicate with Genesis database")
+                        .version(projectVersion)
+                );
+    }
 
-  private OpenAPI generateOpenAPI(){
-    return new OpenAPI()
-            .addServersItem(new Server().url("/"))
-            .info(new Info()
-                    .title("Genesis API")
-                    .description("Rest Endpoints and services to communicate with Genesis database")
-                    .version(projectVersion)
-            );
-  }
-
+    private OAuthFlows getFlows(String authUrl) {
+        OAuthFlows flows = new OAuthFlows();
+        OAuthFlow flow = new OAuthFlow();
+        Scopes scopes = new Scopes();
+        flow.setAuthorizationUrl(authUrl + "/auth");
+        flow.setTokenUrl(authUrl + "/token");
+        flow.setRefreshUrl(authUrl + "/token");
+        flow.setScopes(scopes);
+        return flows.authorizationCode(flow);
+    }
 
 }
