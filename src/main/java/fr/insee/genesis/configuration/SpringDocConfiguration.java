bfb7f67c44a6a10744c52b300f26c535816bd428
Loïc Henninger <loic.henninger@insee.fr>
2024-12-12 15:30:47 +0100

    Remove SpringDocConfiguration tests

--- src/main/java/fr/insee/genesis/configuration/SpringDocConfiguration.java
+++ src/main/java/fr/insee/genesis/configuration/SpringDocConfiguration.java
@@ -3,7 +3,12 @@ package fr.insee.genesis.configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.*;
+
+import io.swagger.v3.oas.models.security.OAuthFlow;
+import io.swagger.v3.oas.models.security.OAuthFlows;
+import io.swagger.v3.oas.models.security.Scopes;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
