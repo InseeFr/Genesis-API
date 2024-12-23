e3febc03f8c6e5e51adf7576498819b0aa268a0f
QDIBYS <alexis.szmundy@insee.fr>
2024-12-23 11:05:00 +0100

    Add already existing compound indexes

--- src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
+++ src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
@@ -2,6 +2,7 @@ package fr.insee.genesis.infrastructure.document.surveyunit;
 
 import fr.insee.genesis.Constants;
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -9,6 +10,9 @@ import java.util.List;
 
 @Data
 @Document(collection = Constants.MONGODB_RESPONSE_COLLECTION_NAME)
+@CompoundIndex(name = "idCampaign_1_idQuestionnaire_1", def = "{'idCampaign': 1, 'idQuestionnaire': 1}")
+@CompoundIndex(name = "idQuestionnaire_1_idCampaign_1", def = "{'idQuestionnaire': 1, 'idCampaign': 1}")
+@CompoundIndex(name = "idQuestionnaire_1_idUE_1", def = "{'idQuestionnaire': 1, 'idUE': 1}")
 public class SurveyUnitDocument {
 	private String idCampaign;
 	private String idUE;
