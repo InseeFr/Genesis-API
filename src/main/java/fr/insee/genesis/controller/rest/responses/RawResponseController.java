fd5dff3728a7274a38bfb8b6f02bd239de78b199
Loïc Henninger <loic.henninger@insee.fr>
2025-03-07 14:33:55 +0100

    fix: can't see endpoints in swagger

--- src/main/java/fr/insee/genesis/controller/rest/responses/RawResponseController.java
+++ src/main/java/fr/insee/genesis/controller/rest/responses/RawResponseController.java
@@ -16,16 +16,20 @@ import fr.insee.genesis.infrastructure.utils.FileUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@Controller
+@RequestMapping(path = "/" )
 public class RawResponseController {
 
     private static final String SUCCESS_MESSAGE = "Data saved";
