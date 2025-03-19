ce24d45c3aea14b0354010dc0cbd3ecc055fb28c
Loïc Henninger <loic.henninger@insee.fr>
2025-03-19 16:54:33 +0100

    feat: adapt existing processing method

--- src/main/java/fr/insee/genesis/domain/ports/api/LunaticJsonRawDataApiPort.java
+++ src/main/java/fr/insee/genesis/domain/ports/api/LunaticJsonRawDataApiPort.java
@@ -14,11 +14,5 @@ public interface LunaticJsonRawDataApiPort {
     List<LunaticJsonRawDataModel> getRawData(String campaignName, Mode mode, List<String> interrogationIdList);
     List<SurveyUnitModel> convertRawData(List<LunaticJsonRawDataModel> rawData, VariablesMap variablesMap);
     List<LunaticJsonRawDataUnprocessedDto> getUnprocessedDataIds();
-    List<SurveyUnitModel> parseRawData(
-            String campaignName,
-            Mode mode,
-            List<String> interrogationIdList,
-            VariablesMap variablesMap
-    );
     void updateProcessDates(List<SurveyUnitModel> surveyUnitModels);
 }
