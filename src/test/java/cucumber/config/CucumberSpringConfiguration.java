b4cf2e0b0f3d1497a43ce5778765aa9f6dca4ad5
Alice Lambois <alice.lambois@insee.fr>
2025-03-20 10:43:04 +0100

    test: add profile for cucumber

--- src/test/java/cucumber/config/CucumberSpringConfiguration.java
+++ src/test/java/cucumber/config/CucumberSpringConfiguration.java
@@ -4,9 +4,11 @@ package cucumber.config;
 import fr.insee.genesis.GenesisApi;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = GenesisApi.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CucumberSpringConfiguration {
 }
 
