83a5232c07f379e4433c97d676dd4484ff4d548d
QDIBYS <alexis.szmundy@insee.fr>
2025-03-28 11:31:26 +0100

    fix(cucumber-tests): Fixed spring security not disabled properly during cucumber tests

--- src/test/java/cucumber/config/CucumberSpringConfiguration.java
+++ src/test/java/cucumber/config/CucumberSpringConfiguration.java
@@ -8,17 +8,18 @@ import fr.insee.genesis.infrastructure.repository.SurveyUnitMongoDBRepository;
 import fr.insee.genesis.infrastructure.repository.VariableTypeMongoDBRepository;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
-@CucumberContextConfiguration
 @SpringBootTest(classes = GenesisApi.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("test-cucumber")
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@ImportAutoConfiguration(exclude = {SecurityAutoConfiguration.class, OAuth2ResourceServerAutoConfiguration.class})
 public class CucumberSpringConfiguration {
     @MockitoBean
     private MongoTemplate mongoTemplate;
--- src/test/java/cucumber/functional_tests/MainDefinitions.java
+++ src/test/java/cucumber/functional_tests/MainDefinitions.java
@@ -36,6 +36,7 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import org.assertj.core.api.Assertions;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -52,6 +53,7 @@ import java.util.stream.Stream;
 
 @CucumberContextConfiguration
 @SpringBootTest(classes = CucumberSpringConfiguration.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test-cucumber")
 public class MainDefinitions {
     String directory;
     Path inDirectory = Paths.get(TestConstants.FUNCTIONAL_TESTS_WEB_DIRECTORY);
