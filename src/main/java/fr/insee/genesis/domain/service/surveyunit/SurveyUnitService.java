176bae310e22d42e6b9b288936a6b87b3884111a
Loïc Henninger <loic.henninger@insee.fr>
2025-06-23 14:40:06 +0200

    chore: sonar code smell

--- src/main/java/fr/insee/genesis/domain/service/surveyunit/SurveyUnitService.java
+++ src/main/java/fr/insee/genesis/domain/service/surveyunit/SurveyUnitService.java
@@ -155,14 +155,8 @@ public class SurveyUnitService implements SurveyUnitApiPort {
         List<List<SurveyUnitModel>> listLatestUpdatesbyVariables = new ArrayList<>();
 
         //1) QUERY
-        // => convertion of "List<InterrogationId>" -> "List<String>" for query using lamda
-        /*
-        List<String> queryInParam = new ArrayList<String>();
-        for(InterrogationId interrId : interrogationIds) {
-            queryInParam.add(interrId.getInterrogationId());
-        }
-        */
-        List<String> queryInParam = interrogationIds.stream().map(InterrogationId::getInterrogationId).collect(Collectors.toList());
+        // => conversion of "List<InterrogationId>" -> "List<String>" for query using lamda
+        List<String> queryInParam = interrogationIds.stream().map(InterrogationId::getInterrogationId).toList();
 
         //Get !!!all versions!!! of a set of "interrogationIds"
         List<SurveyUnitModel> allResponsesVersionsSet = surveyUnitPersistencePort.findBySetOfIdsAndQuestionnaireIdAndMode(questionnaireId, mode, queryInParam);
