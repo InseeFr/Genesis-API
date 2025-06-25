aeb5c0d9e9e430e08592035231b86b7818b0286c
Adrien MARCHAL <adrien.marchal-ext@insee.fr>
2025-06-25 14:10:37 +0200

    fix: add MongoDB indexes

--- src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
+++ src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
@@ -3,6 +3,7 @@ package fr.insee.genesis.infrastructure.document.surveyunit;
 import fr.insee.genesis.Constants;
 import lombok.Data;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -13,11 +14,14 @@ import java.util.List;
 @CompoundIndex(name = "campaignId_1_questionnaireId_1", def = "{'campaignId': 1, 'questionnaireId': 1}") //1 = ascending, -1 = descending
 @CompoundIndex(name = "questionnaireId_1_campaignId_1", def = "{'questionnaireId': 1, 'campaignId': 1}")
 @CompoundIndex(name = "questionnaireId_1_interrogationId_1", def = "{'questionnaireId': 1, 'interrogationId': 1}")
+@CompoundIndex(name = "interrogationId_1_questionnaireId_1", def = "{'interrogationId': 1, 'questionnaireId': 1}")
 public class SurveyUnitDocument {
 	private String campaignId;
+	@Indexed
 	private String interrogationId;
 	private String questionnaireId;
 	private String state;
+	@Indexed
 	private String mode;
 	private LocalDateTime recordDate;
 	private LocalDateTime fileDate;
