7a76730451eda4ec75aadf1eb93e7adf4df4f7dd
amarchal-insee <adrien.marchal-ext@insee.fr>
2025-06-25 12:10:34 +0200

    Update SurveyUnitDocument.java
    add some indexes in MongoDB responses collection for perf. issues from Perret

--- src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
+++ src/main/java/fr/insee/genesis/infrastructure/document/surveyunit/SurveyUnitDocument.java
@@ -3,6 +3,7 @@ package fr.insee.genesis.infrastructure.document.surveyunit;
 import fr.insee.genesis.Constants;
 import lombok.Data;
 import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
@@ -13,13 +14,12 @@ import java.util.List;
 @CompoundIndex(name = "campaignId_1_questionnaireId_1", def = "{'campaignId': 1, 'questionnaireId': 1}") //1 = ascending, -1 = descending
 @CompoundIndex(name = "questionnaireId_1_campaignId_1", def = "{'questionnaireId': 1, 'campaignId': 1}")
 @CompoundIndex(name = "questionnaireId_1_interrogationId_1", def = "{'questionnaireId': 1, 'interrogationId': 1}")
+@CompoundIndex(name = "interrogationId_1_questionnaireId_1", def = "{'interrogationId': 1, 'questionnaireId': 1}")
 public class SurveyUnitDocument {
 	private String campaignId;
+	@Indexed
 	private String interrogationId;
 	private String questionnaireId;
-	private String contextualId;
-	private Boolean isCapturedIndirectly;
-	private LocalDateTime validationDate;
 	private String state;
 	private String mode;
 	private LocalDateTime recordDate;
