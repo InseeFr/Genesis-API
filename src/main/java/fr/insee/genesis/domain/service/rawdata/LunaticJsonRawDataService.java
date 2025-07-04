b49df25c42ac64bf76d835e1fe13bf397a886c0d
Marc BERGER <marc.berger@insee.fr>
2025-07-04 15:04:49 +0200

    feat: add /responses/raw/lunatic-json/{campaignId} endpoint

--- src/main/java/fr/insee/genesis/domain/service/rawdata/LunaticJsonRawDataService.java
+++ src/main/java/fr/insee/genesis/domain/service/rawdata/LunaticJsonRawDataService.java
@@ -24,14 +24,13 @@ import fr.insee.genesis.infrastructure.utils.FileUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Service
 @Slf4j
@@ -337,4 +336,12 @@ public class LunaticJsonRawDataService implements LunaticJsonRawDataApiPort {
     public long countResponsesByQuestionnaireId(String campaignId) {
         return lunaticJsonRawDataPersistencePort.countResponsesByQuestionnaireId(campaignId);
     }
+
+
+    @Override
+    public Page<LunaticJsonRawDataModel> findRawDataByCampaignIdAndDate(String campaignId, Instant startDt, Instant endDt, Pageable pageable){
+        return lunaticJsonRawDataPersistencePort.findByCampaignIdAndDate(campaignId,startDt, endDt,pageable);
+
+    }
+
 }
