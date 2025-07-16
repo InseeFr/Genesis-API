e09472bdfa7e63798d529a5d3ff3ff80b3f342cf
Alice Lambois <alice.lambois@insee.fr>
2025-07-16 11:23:45 +0200

    Fix tests with multitenant auth

--- src/test/java/fr/insee/genesis/controller/rest/ControllerAccessTest.java
+++ src/test/java/fr/insee/genesis/controller/rest/ControllerAccessTest.java
@@ -15,7 +15,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
@@ -23,29 +22,26 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
 import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.oneOf;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
 import static org.springframework.http.HttpMethod.GET;
 import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.jwt;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -53,24 +49,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @EnableAutoConfiguration(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
 class ControllerAccessTest {
 
-    // Constants for user roles
-    private static final String USER_KRAFTWERK = "USER_KRAFTWERK";
-    private static final String USER_PLATINE = "USER_PLATINE";
-    private static final String ADMIN = "ADMIN";
-    private static final String READER = "READER";
-    // JWT claim properties loaded from application properties
-    @Value("${fr.insee.genesis.security.token.oidc-claim-role}")
-    private String claimRoleDotRoles;
-    @Value("${fr.insee.genesis.security.token.oidc-claim-username}")
-    private String claimName;
     @Autowired
     private MockMvc mockMvc; // Simulates HTTP requests to the REST endpoints
-    @MockitoBean
-    private JwtDecoder jwtDecoder;
-    @MockitoBean
-    private MongoTemplate mongoTemplate;
+
     @MockitoBean
     private DataProcessingContextApiPort dataProcessingContextApiPort;
+
+/** MOCKS for initializing context, not used **/
+    @MockitoBean
+    private MongoTemplate mongoTemplate;
     @MockitoBean
     private SurveyUnitApiPort surveyUnitApiPort;
     @MockitoBean
@@ -122,9 +109,10 @@ class ControllerAccessTest {
     @MethodSource("endpointsReader")
     @DisplayName("Admins should access reader-allowed services")
     void admin_should_access_reader_allowed_services(String endpointURI) throws Exception{
-        Jwt jwt = generateJwt(List.of("administrateur_traiter"), ADMIN);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get(endpointURI).header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(
+                        get(endpointURI).with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN")))
+                )
                 .andExpect(status().is(oneOf(200,404)));
     }
 
@@ -135,10 +123,11 @@ class ControllerAccessTest {
     @MethodSource("endpointsReader")
     @DisplayName("Kraftwerk users should access reader-allowed services")
     void kraftwerk_users_should_access_reader_allowed_services(String endpointURI) throws Exception {
-        Jwt jwt = generateJwt(List.of("utilisateur_Kraftwerk"), USER_KRAFTWERK);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get(endpointURI).header("Authorization", "bearer token_blabla"))
-                .andExpect(status().is(oneOf(200, 404)));
+        mockMvc.perform(
+                        get(endpointURI).with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_USER_KRAFTWERK")))
+                )
+                .andExpect(status().is(oneOf(200,404)));
     }
 
     /**
@@ -148,10 +137,11 @@ class ControllerAccessTest {
     @MethodSource("endpointsReader")
     @DisplayName("Platine users should access reader-allowed services")
     void platine_users_should_access_reader_allowed_services(String endpointURI) throws Exception {
-        Jwt jwt = generateJwt(List.of("utilisateur_Platine"), USER_PLATINE);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get(endpointURI).header("Authorization", "bearer token_blabla"))
-                .andExpect(status().is(oneOf(200, 404)));
+        mockMvc.perform(
+                        get(endpointURI).with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_USER_PLATINE")))
+                )
+                .andExpect(status().is(oneOf(200,404)));
     }
 
     /**
@@ -161,10 +151,11 @@ class ControllerAccessTest {
     @MethodSource("endpointsReader")
     @DisplayName("Readers should access reader-allowed services")
     void reader_should_access_reader_allowed_services(String endpointURI) throws Exception {
-        Jwt jwt = generateJwt(List.of("lecteur_traiter"), "reader");
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get(endpointURI).header("Authorization", "bearer token_blabla"))
-                .andExpect(status().is(oneOf(200, 404)));
+        mockMvc.perform(
+                        get(endpointURI).with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_READER")))
+                )
+                .andExpect(status().is(oneOf(200,404)));
     }
 
     /**
@@ -174,9 +165,10 @@ class ControllerAccessTest {
     @MethodSource("endpointsReader")
     @DisplayName("User with invalid roles should not access reader-allowed services")
     void invalid_user_should_not_access_reader_allowed_services(String endpointURI) throws Exception {
-        Jwt jwt = generateJwt(List.of("toto"), "invalid_role");
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get(endpointURI).header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(
+                        get(endpointURI).with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_invalid")))
+                )
                 .andExpect(status().isForbidden());
     }
 
@@ -185,11 +177,12 @@ class ControllerAccessTest {
      */
     @Test
     @DisplayName("Reader should access schedule/all endpoint")
-    void reader_should_access_schedules_services() throws Exception {
-        Jwt jwt = generateJwt(List.of("lecteur_traiter"), READER);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get("/context/schedules/all").header("Authorization", "bearer token_blabla"))
-                .andExpect(status().is(oneOf(200, 404)));
+    void reader_should_access_schedules_services() throws Exception{
+        mockMvc.perform(
+                        get("/schedule/all").with(jwt()
+                                .authorities(new SimpleGrantedAuthority("ROLE_READER")))
+                )
+                .andExpect(status().is(oneOf(200,404)));
     }
 
     /**
@@ -199,9 +192,10 @@ class ControllerAccessTest {
     @DisplayName("Reader should not access other schedule endpoints")
     void reader_should_not_access_other_schedules_services() throws Exception {
         doNothing().when(dataProcessingContextApiPort).deleteSchedules(anyString());
-        Jwt jwt = generateJwt(List.of("lecteur_traiter"), READER);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(delete("/context/schedules?partitionId=ENQ_TEST").header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(
+                        delete("/context/schedules?partitionId=ENQ_TEST").with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_READER")))
+                )
                 .andExpect(status().isForbidden());
     }
 
@@ -213,8 +207,6 @@ class ControllerAccessTest {
     @MethodSource("responseEndpoint")
     @DisplayName("Reader should not access /responses endpoints")
     void reader_should_not_access_response_services(HttpMethod method,String endpointURI) throws Exception {
-        Jwt jwt = generateJwt(List.of("lecteur_traiter"), READER);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
         MockHttpServletRequestBuilder requestBuilder;
         if (method == HttpMethod.GET) {
             requestBuilder = get(endpointURI);
@@ -228,8 +220,11 @@ class ControllerAccessTest {
             throw new IllegalArgumentException("Unsupported HTTP method: " + method);
         }
 
-        mockMvc.perform(requestBuilder.header("Authorization", "bearer token_blabla"))
-                .andExpect(status().is(oneOf(200, 404)));
+        mockMvc.perform(
+                        requestBuilder.with(
+                                jwt().authorities(new SimpleGrantedAuthority("ROLE_READER")))
+                )
+               .andExpect(status().is(oneOf(200, 404)));
     }
 
 
@@ -239,9 +234,10 @@ class ControllerAccessTest {
     @Test
     @DisplayName("Kraftwerk users should access schedules service")
     void kraftwerk_users_should_not_access_schedules_services() throws Exception {
-        Jwt jwt = generateJwt(List.of("utilisateur_Kraftwerk"), USER_KRAFTWERK);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get("/context/schedules/all").header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(
+                get("/context/schedules/all").with(
+                        jwt().authorities(new SimpleGrantedAuthority("ROLE_USER_KRAFTWERK"))
+                        ))
                 .andExpect(status().is(oneOf(200, 404)));
     }
 
@@ -251,9 +247,8 @@ class ControllerAccessTest {
     @Test
     @DisplayName("Admins should access schedules service")
     void admins_should_access_schedules_services() throws Exception {
-        Jwt jwt = generateJwt(List.of("administrateur_traiter"), ADMIN);
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get("/context/schedules/all").header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(get("/context/schedules/all").with(
+                jwt().authorities(new SimpleGrantedAuthority("ROLE_ADMIN"))))
                 .andExpect(status().is(oneOf(200, 404)));
     }
 
@@ -263,30 +258,10 @@ class ControllerAccessTest {
     @Test
     @DisplayName("Invalid roles should not access schedules service")
     void invalid_roles_should_access_schedules_services() throws Exception {
-        Jwt jwt = generateJwt(List.of("invalid_role"), "invalid_role");
-        when(jwtDecoder.decode(anyString())).thenReturn(jwt);
-        mockMvc.perform(get("/context/schedules").header("Authorization", "bearer token_blabla"))
+        mockMvc.perform(get("/context/schedules").with(
+                jwt().authorities(new SimpleGrantedAuthority("ROLE_invalid"))))
                 .andExpect(status().isForbidden());
     }
 
-    /**
-     * Generates a mock JWT token with specified roles and username.
-     *
-     * @param roles List of roles assigned to the user.
-     * @param name  Username for the JWT.
-     * @return A mock Jwt object.
-     */
-    public Jwt generateJwt(List<String> roles, String name) {
-        Date issuedAt = new Date();
-        Date expiresAT = Date.from((new Date()).toInstant().plusSeconds(100));
-        var claimRole = claimRoleDotRoles.split("\\.")[0];
-        var attributRole = claimRoleDotRoles.split("\\.")[1];
-        return new Jwt("token", issuedAt.toInstant(), expiresAT.toInstant(),
-                Map.of("alg", "RS256", "typ", "JWT"),
-                Map.of(claimRole, Map.of(attributRole, roles),
-                        claimName, name
-                )
-        );
-    }
 
 }
