bdd8db526a8976108b4af3291db93dad4e4ae6eb
QDIBYS <alexis.szmundy@insee.fr>
2025-08-01 16:14:50 +0200

    fix: add path for questionnaire metadatas

--- src/main/java/fr/insee/genesis/controller/rest/QuestionnaireMetadataController.java
+++ src/main/java/fr/insee/genesis/controller/rest/QuestionnaireMetadataController.java
@@ -13,9 +13,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping(path = "/questionnaire-metadata")
 @AllArgsConstructor
 public class QuestionnaireMetadataController {
 
