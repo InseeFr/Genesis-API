1132f9b6d14a35ed71bc7d84cdbfb14b30360683
Alice Lambois <alice.lambois@insee.fr>
2025-10-27 09:07:22 +0100

    style: remove unused import

--- src/main/java/fr/insee/genesis/controller/rest/JsonExtractionController.java
+++ src/main/java/fr/insee/genesis/controller/rest/JsonExtractionController.java
@@ -8,7 +8,6 @@ import fr.insee.genesis.exceptions.GenesisException;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
--- src/main/java/fr/insee/genesis/domain/ports/api/ContextualPreviousVariableApiPort.java
+++ src/main/java/fr/insee/genesis/domain/ports/api/ContextualPreviousVariableApiPort.java
@@ -3,8 +3,6 @@ package fr.insee.genesis.domain.ports.api;
 import fr.insee.genesis.domain.model.contextualvariable.ContextualPreviousVariableModel;
 import fr.insee.genesis.exceptions.GenesisException;
 
-import java.io.InputStream;
-
 public interface ContextualPreviousVariableApiPort {
     boolean readContextualPreviousFile(String questionnaireId, String sourceState, String filePath) throws GenesisException;
 
