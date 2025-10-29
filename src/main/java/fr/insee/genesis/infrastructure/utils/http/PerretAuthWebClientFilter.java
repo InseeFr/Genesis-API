34433acb3a7af01d87557d7a34934ebe175416ed
Alice Lambois <alice.lambois@insee.fr>
2025-10-29 09:17:45 +0100

    style : change annotation

--- src/main/java/fr/insee/genesis/infrastructure/utils/http/PerretAuthWebClientFilter.java
+++ src/main/java/fr/insee/genesis/infrastructure/utils/http/PerretAuthWebClientFilter.java
@@ -1,7 +1,6 @@
 package fr.insee.genesis.infrastructure.utils.http;
 
-import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.function.client.ClientRequest;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
@@ -10,7 +9,7 @@ import reactor.core.publisher.Mono;
 import java.io.IOException;
 
 @Slf4j
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@UtilityClass
 public class PerretAuthWebClientFilter {
     public static ExchangeFilterFunction perretAuthFilter(OidcService oidcService) {
         return ExchangeFilterFunction.ofRequestProcessor(request -> {
