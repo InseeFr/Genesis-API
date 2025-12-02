f19f02597a446ba016a7412441ece2d24d139fc4
Loïc Henninger <loic.henninger@insee.fr>
2025-12-02 17:59:48 +0100

    fix: missing@Mapper annotation

--- src/main/java/fr/insee/genesis/infrastructure/mappers/LunaticModelMapper.java
+++ src/main/java/fr/insee/genesis/infrastructure/mappers/LunaticModelMapper.java
@@ -3,11 +3,13 @@ package fr.insee.genesis.infrastructure.mappers;
 import fr.insee.genesis.domain.model.lunaticmodel.LunaticModelModel;
 import fr.insee.genesis.infrastructure.document.lunaticmodel.LunaticModelDocument;
 import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
+@Mapper
 public interface LunaticModelMapper {
     LunaticModelMapper INSTANCE = Mappers.getMapper(LunaticModelMapper.class);
 
