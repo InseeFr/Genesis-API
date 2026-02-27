6cb89900e0060dc36b8df57fce9c7317e0e6eb19
Hajarel-moukh <hajar.el-moukh@insee.fr>
2026-02-27 17:44:23 +0100

    modify tests

--- src/main/java/fr/insee/genesis/controller/rest/responses/ContextualVariableController.java
+++ src/main/java/fr/insee/genesis/controller/rest/responses/ContextualVariableController.java
@@ -12,7 +12,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
