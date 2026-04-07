b3c09ad1cabee6834471139149297f10acef215f
QDIBYS <alexis.szmundy@insee.fr>
2026-04-07 16:00:21 +0200

    chore: remove schedule endpoints
    cf. review swagger doc

--- src/main/java/fr/insee/genesis/controller/rest/DataProcessingContextController.java
+++ src/main/java/fr/insee/genesis/controller/rest/DataProcessingContextController.java
@@ -1,11 +1,8 @@
 package fr.insee.genesis.controller.rest;
 
-import fr.insee.genesis.Constants;
 import fr.insee.genesis.controller.dto.KraftwerkExecutionScheduleInput;
-import fr.insee.genesis.controller.dto.ScheduleDto;
 import fr.insee.genesis.controller.dto.ScheduleRequestDto;
 import fr.insee.genesis.controller.dto.rawdata.ScheduleResponseDto;
-import fr.insee.genesis.domain.model.context.schedule.ServiceToCall;
 import fr.insee.genesis.domain.model.context.schedule.TrustParameters;
 import fr.insee.genesis.domain.ports.api.DataProcessingContextApiPort;
 import fr.insee.genesis.exceptions.GenesisException;
@@ -17,7 +14,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.scheduling.support.CronExpression;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -37,23 +33,6 @@ import java.util.List;
 public class DataProcessingContextController {
     private DataProcessingContextApiPort dataProcessingContextApiPort;
     private final FileUtils fileUtils;
-
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Create or update a data processing context")
-    @PutMapping(path = "/context/review")
-    @PreAuthorize("hasAnyRole('USER_PLATINE', 'USER_BACK_OFFICE', 'SCHEDULER')")
-    public ResponseEntity<Object> saveContext(
-            @Parameter(description = "Identifier of the partition", required = true) @RequestParam("partitionId") String partitionId,
-            @Parameter(description = "Allow reviewing") @RequestParam(value = "withReview", defaultValue = "false") Boolean withReview
-    ){
-        try {
-            withReview = withReview != null && withReview; //False if null
-            dataProcessingContextApiPort.saveContext(partitionId, withReview);
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        return ResponseEntity.ok().build();
-    }
 
     @Operation(summary = "Create or update a data processing context")
     @PutMapping(path = "/contexts/{collectionInstrumentId}/review")
@@ -80,21 +59,6 @@ public class DataProcessingContextController {
     ){
         try {
             boolean withReview = dataProcessingContextApiPort.getReviewByCollectionInstrumentId(collectionInstrumentId);
-            return ResponseEntity.ok(withReview);
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-    }
-
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Returns partition review indicator")
-    @GetMapping(path = "/context/review")
-    @PreAuthorize("hasAnyRole('USER_BACK_OFFICE','SCHEDULER','USER_PLATINE')")
-    public ResponseEntity<Object> getReviewIndicator(
-            @Parameter(description = "Identifier of the partition", required = true) @RequestParam("partitionId") String partitionId
-    ){
-        try {
-            boolean withReview = dataProcessingContextApiPort.getReviewByPartitionId(partitionId);
             return ResponseEntity.ok(withReview);
         }catch (GenesisException e){
             return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
@@ -139,98 +103,6 @@ public class DataProcessingContextController {
         } catch (GenesisException e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
         }
-    }
-
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Schedule a Kraftwerk execution")
-    @PutMapping(path = "/context/schedules")
-    @PreAuthorize("hasRole('USER_KRAFTWERK')")
-    public ResponseEntity<Object> saveSchedule(
-            @Parameter(description = "Partition identifier to call Kraftwerk on") @RequestParam("partitionId") String partitionId,
-            @Parameter(description = "Kraftwerk endpoint") @RequestParam(value = "serviceTocall", defaultValue = Constants.KRAFTWERK_MAIN_ENDPOINT) ServiceToCall serviceToCall,
-            @Parameter(description = "Frequency in Spring cron format (6 inputs, go to https://crontab.cronhub.io/ for generator)  \n Example : 0 0 6 * * *") @RequestParam("frequency") String frequency,
-            @Parameter(description = "Schedule effective date and time", example = "2024-01-01T12:00:00") @RequestParam("scheduleBeginDate") LocalDateTime scheduleBeginDate,
-            @Parameter(description = "Schedule end date and time", example = "2024-01-01T12:00:00") @RequestParam("scheduleEndDate") LocalDateTime scheduleEndDate,
-            @Parameter(description = "Encrypt after process ? Ignore next parameters if false") @RequestParam(value =
-                    "useEncryption",
-                    defaultValue = "false") boolean useEncryption,
-            @Parameter(description = "(Encryption) vault path") @RequestParam(value = "encryptionVaultPath", defaultValue = "") String encryptionVaultPath,
-            @Parameter(description = "(Encryption) output folder") @RequestParam(value = "encryptionOutputFolder",
-                    defaultValue = "") String encryptionOutputFolder,
-            @Parameter(description = "(Encryption) Use signature system") @RequestParam(value = "useSignature", defaultValue = "false") boolean useSignature
-    ) {
-        try {
-            //Check frequency
-            if(!CronExpression.isValidExpression(frequency)) {
-                log.warn("Returned error for wrong frequency : {}", frequency);
-                throw new GenesisException(400, "Wrong frequency syntax");
-            }
-
-            TrustParameters trustParameters = null;
-            if(useEncryption) {
-                trustParameters = new TrustParameters(
-                        fileUtils.getKraftwerkOutFolder(partitionId),
-                        encryptionOutputFolder,
-                        encryptionVaultPath,
-                        useSignature
-                );
-            }
-            dataProcessingContextApiPort.saveKraftwerkExecutionSchedule(
-                    partitionId,
-                    serviceToCall == null ? ServiceToCall.MAIN : serviceToCall,
-                    frequency,
-                    scheduleBeginDate, scheduleEndDate, trustParameters
-            );
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    // Should be refactored to make it restfull
-    @Operation(summary = "Schedule a Kraftwerk execution using the collection instrument")
-    @PutMapping(path = "/contexts/schedules")
-    @PreAuthorize("hasRole('USER_KRAFTWERK')")
-    public ResponseEntity<Object> saveScheduleWithCollectionInstrumentId(
-            @Parameter(description = "Collection instrument to call Kraftwerk on") @RequestParam("collectionInstrumentId") String collectionInstrumentId,
-            @Parameter(description = "Kraftwerk endpoint") @RequestParam(value = "serviceTocall", defaultValue = Constants.KRAFTWERK_MAIN_ENDPOINT) ServiceToCall serviceToCall,
-            @Parameter(description = "Frequency in Spring cron format (6 inputs, go to https://crontab.cronhub.io/ for generator)  \n Example : 0 0 6 * * *") @RequestParam("frequency") String frequency,
-            @Parameter(description = "Schedule effective date and time", example = "2024-01-01T12:00:00") @RequestParam("scheduleBeginDate") LocalDateTime scheduleBeginDate,
-            @Parameter(description = "Schedule end date and time", example = "2024-01-01T12:00:00") @RequestParam("scheduleEndDate") LocalDateTime scheduleEndDate,
-            @Parameter(description = "Encrypt after process ? Ignore next parameters if false") @RequestParam(value =
-                    "useEncryption",
-                    defaultValue = "false") boolean useEncryption,
-            @Parameter(description = "(Encryption) vault path") @RequestParam(value = "encryptionVaultPath", defaultValue = "") String encryptionVaultPath,
-            @Parameter(description = "(Encryption) output folder") @RequestParam(value = "encryptionOutputFolder",
-                    defaultValue = "") String encryptionOutputFolder,
-            @Parameter(description = "(Encryption) Use signature system") @RequestParam(value = "useSignature", defaultValue = "false") boolean useSignature
-    ) {
-        try {
-            //Check frequency
-            if(!CronExpression.isValidExpression(frequency)) {
-                log.warn("Returned error for wrong frequency : {}", frequency);
-                throw new GenesisException(400, "Wrong frequency syntax");
-            }
-
-            TrustParameters trustParameters = null;
-            if(useEncryption) {
-                trustParameters = new TrustParameters(
-                        fileUtils.getKraftwerkOutFolder(collectionInstrumentId),
-                        encryptionOutputFolder,
-                        encryptionVaultPath,
-                        useSignature
-                );
-            }
-            dataProcessingContextApiPort.saveKraftwerkExecutionScheduleByCollectionInstrumentId(
-                    collectionInstrumentId,
-                    serviceToCall == null ? ServiceToCall.MAIN : serviceToCall,
-                    frequency,
-                    scheduleBeginDate, scheduleEndDate, trustParameters
-            );
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Update a Kraftwerk execution schedule V2")
@@ -278,32 +150,6 @@ public class DataProcessingContextController {
         return ResponseEntity.ok().build();
     }
 
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Fetch all schedules")
-    @GetMapping(path = "/context/schedules")
-    @PreAuthorize("hasAnyRole('SCHEDULER','READER')")
-    public ResponseEntity<Object> getAllSchedules() {
-        log.debug("Got GET all schedules request");
-
-        List<ScheduleDto> surveyScheduleDocumentModels = dataProcessingContextApiPort.getAllSchedules();
-
-        log.info("Returning {} schedule documents...", surveyScheduleDocumentModels.size());
-        return ResponseEntity.ok(surveyScheduleDocumentModels);
-    }
-
-    //It is just a change of path in the url
-    @Operation(summary = "Fetch all schedules")
-    @GetMapping(path = "/contexts/schedules")
-    @PreAuthorize("hasAnyRole('SCHEDULER','READER')")
-    public ResponseEntity<Object> getAllSchedulesV2() {
-        log.debug("Got GET all schedules request");
-
-        List<ScheduleDto> surveyScheduleDocumentModels = dataProcessingContextApiPort.getAllSchedules();
-
-        log.info("Returning {} schedule documents...", surveyScheduleDocumentModels.size());
-        return ResponseEntity.ok(surveyScheduleDocumentModels);
-    }
-
     @Operation(summary = "Fetch all schedules V2")
     @GetMapping(path = "/contexts/schedules/v2")
     @PreAuthorize("hasAnyRole('SCHEDULER','READER')")
@@ -326,55 +172,6 @@ public class DataProcessingContextController {
                 dataProcessingContextApiPort.getSchedulesV2ByCollectionInstrumentId(collectionInstrumentId);
 
         return ResponseEntity.ok(schedules);
-    }
-
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Set last execution date of a partition with new date or nothing")
-    @PostMapping(path = "/context/schedules/lastExecutionDate")
-    @PreAuthorize("hasRole('SCHEDULER')")
-    public ResponseEntity<Object> setSurveyLastExecution(
-            @Parameter(description = "Survey name to call Kraftwerk on") @RequestBody String partitionId,
-            @Parameter(description = "Date to save as last execution date", example = "2024-01-01T12:00:00") @RequestParam("newDate") LocalDateTime newDate
-    ) {
-        try {
-            dataProcessingContextApiPort.updateLastExecutionDate(partitionId, newDate);
-            log.info("{} last execution updated at {} !", partitionId, newDate);
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @Operation(summary = "Update the date of the last extraction of data corresponding to a collection instrument")
-    @PutMapping(path = "/contexts/{collectionInstrumentId}/lastExecutionDate")
-    @PreAuthorize("hasRole('SCHEDULER')")
-    public ResponseEntity<Object> setSurveyLastExecutionByCollectionInstrumentId(
-            @PathVariable("collectionInstrumentId") @RequestBody String collectionInstrumentId,
-            @Parameter(description = "Date to save as last execution date", example = "2024-01-01T12:00:00") @RequestParam("newDate") LocalDateTime newDate
-    ) {
-        try {
-            dataProcessingContextApiPort.updateLastExecutionDateByCollectionInstrumentId(collectionInstrumentId, newDate);
-            log.info("{} last execution updated at {} !", collectionInstrumentId, newDate);
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        return ResponseEntity.ok().build();
-    }
-
-    @Deprecated(forRemoval = true)
-    @Operation(summary = "Delete the Kraftwerk execution schedules of a partition")
-    @DeleteMapping(path = "/context/schedules")
-    @PreAuthorize("hasRole('USER_KRAFTWERK')")
-    public ResponseEntity<Object> deleteSchedules(
-            @Parameter(description = "Survey name of the schedule(s) to delete") @RequestParam("partitionId") String partitionId
-    ){
-        try {
-            dataProcessingContextApiPort.deleteSchedules(partitionId);
-        }catch (GenesisException e){
-            return new ResponseEntity<>(e.getMessage(), HttpStatusCode.valueOf(e.getStatus()));
-        }
-        log.info("Schedule deleted for survey {}", partitionId);
-        return ResponseEntity.ok().build();
     }
 
     @Operation(summary = "Delete the Kraftwerk execution schedules of a collection instrument id")
--- src/main/java/fr/insee/genesis/domain/ports/api/DataProcessingContextApiPort.java
+++ src/main/java/fr/insee/genesis/domain/ports/api/DataProcessingContextApiPort.java
@@ -4,8 +4,6 @@ import fr.insee.genesis.controller.dto.KraftwerkExecutionScheduleInput;
 import fr.insee.genesis.controller.dto.ScheduleDto;
 import fr.insee.genesis.controller.dto.rawdata.ScheduleResponseDto;
 import fr.insee.genesis.domain.model.context.DataProcessingContextModel;
-import fr.insee.genesis.domain.model.context.schedule.ServiceToCall;
-import fr.insee.genesis.domain.model.context.schedule.TrustParameters;
 import fr.insee.genesis.exceptions.GenesisException;
 
 import java.time.LocalDateTime;
@@ -15,29 +13,9 @@ public interface DataProcessingContextApiPort {
     void saveContext(String partitionId, Boolean withReview) throws GenesisException;
     void saveContextByCollectionInstrumentId(String collectionInstrumentID, Boolean withReview) throws GenesisException;
 
-    @Deprecated(forRemoval = true)
-    void saveKraftwerkExecutionSchedule(String partitionId,
-                                        ServiceToCall serviceToCall,
-                                        String frequency,
-                                        LocalDateTime startDate,
-                                        LocalDateTime endDate,
-                                        TrustParameters trustParameters) throws GenesisException;
-
-    void saveKraftwerkExecutionScheduleByCollectionInstrumentId(String collectionInstrumentId,
-                                        ServiceToCall serviceToCall,
-                                        String frequency,
-                                        LocalDateTime startDate,
-                                        LocalDateTime endDate,
-                                        TrustParameters trustParameters) throws GenesisException;
-
     String createKraftwerkExecutionSchedule(KraftwerkExecutionScheduleInput scheduleInput) throws GenesisException;
 
     void updateKraftwerkExecutionSchedule(KraftwerkExecutionScheduleInput scheduleInput) throws GenesisException;
-
-    void updateLastExecutionDate(String surveyName, LocalDateTime newDate) throws GenesisException;
-    void updateLastExecutionDateByCollectionInstrumentId(String collectionInstrumentId, LocalDateTime newDate) throws GenesisException;
-
-    void deleteSchedules(String surveyName) throws GenesisException;
 
     void deleteScheduleV2(String collectionInstrumentId, String scheduleUuid) throws GenesisException;
 
@@ -59,18 +37,7 @@ public interface DataProcessingContextApiPort {
 
     DataProcessingContextModel getContextByCollectionInstrumentId(String collectionInstrumentId);
 
-    @Deprecated(forRemoval = true)
-    List<String> getPartitionIds(boolean withReview);
-
     List<String> getCollectionInstrumentIds(boolean withReview);
-
-    /**
-     * Gets the review indicator for a partition
-     * @param partitionId id of the partition
-     * @return the review indicator stored in genesis
-     */
-    @Deprecated(forRemoval = true)
-    boolean getReviewByPartitionId(String partitionId) throws GenesisException;
 
     boolean getReviewByCollectionInstrumentId(String collectionInstrumentId) throws GenesisException;
 
