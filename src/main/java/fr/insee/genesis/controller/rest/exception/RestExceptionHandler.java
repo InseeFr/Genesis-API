407fae7032a6ec0fbdf33da557a7168754a6e183
nsenave <nicolas.senave@gmail.com>
2026-04-15 18:03:35 +0200

    refactor: wip still in progress

---
+++ src/main/java/fr/insee/genesis/controller/rest/exception/RestExceptionHandler.java
@@ -0,0 +1,10 @@
+package fr.insee.genesis.controller.rest.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    // TODO: put all exception handlers here, make them return problem detail object, then delete other classes
+
+}
