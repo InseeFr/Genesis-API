8665103cd08280c4fdc0599acfcf754e29ee57b5
Alice Lambois <alice.lambois@insee.fr>
2026-04-27 15:36:34 +0200

    chore: log user

--- src/main/java/fr/insee/genesis/configuration/LogRequestFilter.java
+++ src/main/java/fr/insee/genesis/configuration/LogRequestFilter.java
@@ -7,6 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
@@ -21,9 +24,8 @@ import java.io.IOException;
 @Slf4j
 public class LogRequestFilter extends OncePerRequestFilter {
 	
-	private static final String REQUEST_MESSAGE_FORMAT = 
-			 "CALL {} {} - "
-			+ "Params : {}";
+	private static final String REQUEST_MESSAGE_FORMAT =
+            "CALL {} {} - User: {} - Params : {}";
 	
 	private static final String RESPONSE_MESSAGE_FORMAT = 
 			 "END {} {}  - "
@@ -37,11 +39,21 @@ public class LogRequestFilter extends OncePerRequestFilter {
         ContentCachingRequestWrapper req = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper resp = new ContentCachingResponseWrapper(response);
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        String username = "anonymous";
+
+        if (authentication instanceof JwtAuthenticationToken jwtAuth) {
+            username = jwtAuth.getToken().getClaimAsString("preferred_username");
+        } else if (authentication != null) {
+            username = authentication.getName();
+        }
+
         log.info(REQUEST_MESSAGE_FORMAT,
-        		req.getMethod(), req.getRequestURI(),
-        	//	req.getContentType(),
-            //    new ServletServerHttpRequest(req).getHeaders(), //Headers
-                request.getQueryString());//Params
+                req.getMethod(),
+                req.getRequestURI(),
+                username,
+                request.getQueryString());
 
 
         // Execution request chain
