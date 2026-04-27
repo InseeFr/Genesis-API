ed60b50ef44f92fe81187211bfc982684c76191b
Alice Lambois <alice.lambois@insee.fr>
2026-04-27 16:09:07 +0200

    fix: try to log better

---
+++ src/main/java/fr/insee/genesis/configuration/LogInterceptor.java
@@ -0,0 +1,49 @@
+package fr.insee.genesis.configuration;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.UUID;
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class LogInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String fishTag = UUID.randomUUID().toString();
+        String method = request.getMethod();
+        String operationPath = request.getRequestURI();
+        Authentication authentication = getCurrentUser();
+        ThreadContext.put("user", authentication.getName().toUpperCase());
+        ThreadContext.put("id", fishTag);
+        ThreadContext.put("path", operationPath);
+        ThreadContext.put("method", method);
+        log.info("[" + authentication.getName().toUpperCase() + "] - [" + method + "] - [" + operationPath + "]");
+        return true;
+    }
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView mv) {
+        // no need to posthandle things for this interceptor
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                Exception exception) throws Exception {
+        ThreadContext.clearMap();
+    }
+
+
+    public Authentication getCurrentUser(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+}
+
