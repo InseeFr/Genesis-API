cd67b0bd0d097fbb8b1a9be6d629a3645b850e78
Hajarel-moukh <hajar.el-moukh@insee.fr>
2026-06-08 14:06:26 +0200

    fix: delete only expired V2 schedules from dataProcessingContext (#477)
    * fix: delete only expired V2 schedules from dataProcessingContext
    
    * update delete logic to delete v1 and V2 schedules
    
    * delete unused import and method
    
    * remove only matching expired V1 schedules

--- src/main/java/fr/insee/genesis/infrastructure/adapter/DataProcessingContextMongoAdapter.java
+++ src/main/java/fr/insee/genesis/infrastructure/adapter/DataProcessingContextMongoAdapter.java
@@ -2,7 +2,9 @@ package fr.insee.genesis.infrastructure.adapter;
 
 import fr.insee.genesis.Constants;
 import fr.insee.genesis.domain.model.context.DataProcessingContextModel;
+import fr.insee.genesis.domain.model.context.schedule.DeletedExpiredSchedules;
 import fr.insee.genesis.domain.model.context.schedule.KraftwerkExecutionSchedule;
+import fr.insee.genesis.domain.model.context.schedule.KraftwerkExecutionScheduleV2;
 import fr.insee.genesis.domain.ports.spi.DataProcessingContextPersistancePort;
 import fr.insee.genesis.infrastructure.document.context.DataProcessingContextDocument;
 import fr.insee.genesis.infrastructure.mappers.DataProcessingContextMapper;
@@ -16,10 +18,9 @@ import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Qualifier("dataProcessingContextMongoAdapter")
@@ -75,23 +76,66 @@ public class DataProcessingContextMongoAdapter implements DataProcessingContextP
     }
 
     @Override
-    public List<KraftwerkExecutionSchedule> removeExpiredSchedules(DataProcessingContextModel dataProcessingContextModel) throws IOException {
-        //TODO move non mongo related logic to service
-        List<KraftwerkExecutionSchedule> deletedKraftwerkExecutionSchedules = new ArrayList<>();
-        for (KraftwerkExecutionSchedule kraftwerkExecutionScheduleToRemove :
-                dataProcessingContextModel.getKraftwerkExecutionScheduleList().stream().filter(
-                        kraftwerkExecutionSchedule -> kraftwerkExecutionSchedule.getScheduleEndDate().isBefore(LocalDateTime.now())
-                ).toList()) {
-            deletedKraftwerkExecutionSchedules.add(kraftwerkExecutionScheduleToRemove);
-            Query query =
-                    Query.query(Criteria.where("scheduleEndDate").is(kraftwerkExecutionScheduleToRemove.getScheduleEndDate()));
-            if (dataProcessingContextModel.getCollectionInstrumentId() != null){
-                mongoTemplate.updateMulti(Query.query(Criteria.where("collectionInstrumentId").is(dataProcessingContextModel.getCollectionInstrumentId())), new Update().pull(
-                                "kraftwerkExecutionScheduleList", query),
-                        Constants.MONGODB_SCHEDULE_COLLECTION_NAME);
-            }
+    public DeletedExpiredSchedules removeExpiredSchedules(DataProcessingContextModel context) {
+        LocalDateTime now = LocalDateTime.now();
+
+        List<KraftwerkExecutionSchedule> deletedV1 =
+                Optional.ofNullable(context.getKraftwerkExecutionScheduleList())
+                        .orElse(List.of())
+                        .stream()
+                        .filter(schedule -> schedule.getScheduleEndDate() != null)
+                        .filter(schedule -> schedule.getScheduleEndDate().isBefore(now))
+                        .toList();
+
+        List<KraftwerkExecutionScheduleV2> deletedV2 =
+                Optional.ofNullable(context.getKraftwerkExecutionScheduleV2List())
+                        .orElse(List.of())
+                        .stream()
+                        .filter(schedule -> schedule.getScheduleEndDate() != null)
+                        .filter(schedule -> schedule.getScheduleEndDate().isBefore(now))
+                        .toList();
+
+        Query query = Query.query(
+                Criteria.where("collectionInstrumentId").is(context.getCollectionInstrumentId())
+        );
+
+        for (KraftwerkExecutionSchedule scheduleToRemove : deletedV1) {
+            Update update = new Update().pull(
+                    "kraftwerkExecutionScheduleList",
+                    Query.query(
+                            Criteria.where("partitionId").is(scheduleToRemove.getPartitionId())
+                                    .and("frequency").is(scheduleToRemove.getFrequency())
+                                    .and("serviceToCall").is(scheduleToRemove.getServiceToCall())
+                                    .and("scheduleBeginDate").is(scheduleToRemove.getScheduleBeginDate())
+                                    .and("scheduleEndDate").is(scheduleToRemove.getScheduleEndDate())
+                                    .and("trustParameters").is(scheduleToRemove.getTrustParameters())
+                    ).getQueryObject()
+            );
+
+            mongoTemplate.updateMulti(
+                    query,
+                    update,
+                    Constants.MONGODB_CONTEXT_COLLECTION_NAME
+            );
         }
-        return deletedKraftwerkExecutionSchedules;
+
+        for (KraftwerkExecutionScheduleV2 scheduleToRemove : deletedV2) {
+            Update update = new Update().pull(
+                    "kraftwerkExecutionScheduleV2List",
+                    Query.query(
+                            Criteria.where("scheduleUuid")
+                                    .is(scheduleToRemove.getScheduleUuid())
+                    ).getQueryObject()
+            );
+
+            mongoTemplate.updateMulti(
+                    query,
+                    update,
+                    Constants.MONGODB_CONTEXT_COLLECTION_NAME
+            );
+        }
+
+        return new DeletedExpiredSchedules(deletedV1, deletedV2);
     }
 
     @Override
