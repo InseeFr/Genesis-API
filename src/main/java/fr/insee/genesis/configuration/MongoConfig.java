8a022f72b5929e6ef6d7cb93263bf71b67fd7248
Loïc Henninger <loic.henninger@insee.fr>
2026-06-12 16:29:53 +0200

    fix: add transactional annotation on saveAll

---
+++ src/main/java/fr/insee/genesis/configuration/MongoConfig.java
@@ -0,0 +1,16 @@
+package fr.insee.genesis.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+
+@Configuration
+public class MongoConfig {
+
+    @Bean
+    MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
+    }
+
+}
