e6e07c49d4444ab278f8adfd0abb7e630921c8d6
Loïc Henninger <loic.henninger@insee.fr>
2026-06-19 17:17:16 +0200

    fix:  register MongoTransactionManager reliably in prod environment

--- src/main/java/fr/insee/genesis/configuration/MongoConfig.java
+++ src/main/java/fr/insee/genesis/configuration/MongoConfig.java
@@ -1,16 +1,17 @@
 package fr.insee.genesis.configuration;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class MongoConfig {
 
     @Bean
-    @ConditionalOnBean(MongoDatabaseFactory.class)
+    @ConditionalOnMissingBean(PlatformTransactionManager.class)
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
         return new MongoTransactionManager(dbFactory);
     }
