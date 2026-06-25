6322abf3a0b1d075b3fad94fffe809825081d72e
Hajarel-moukh <hajar.el-moukh@insee.fr>
2026-06-25 16:28:46 +0200

    fix/dto-jackson-deserialization (#509)

--- src/main/java/fr/insee/genesis/controller/dto/VariableInputDto.java
+++ src/main/java/fr/insee/genesis/controller/dto/VariableInputDto.java
@@ -1,9 +1,13 @@
 package fr.insee.genesis.controller.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class VariableInputDto {
